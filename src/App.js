8ee930a348a56991518574ec2652381919df54a8
cosmin199 <cosmin_ionescu_email@yahoo.ro>
2021-08-25 02:00:09 -0700

    fetching data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,56 @@
 import "./App.css"
+import axios from "axios"
+import { useState, useEffect } from "react"
+import Coin from "./Coin"
 
 function App() {
+  const [crypto, setCrypto] = useState([])
+  const [search, setSearch] = useState("")
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      )
+      .then((res) => {
+        setCrypto(res.data)
+      })
+      .catch((error) => alert("yoo error"))
+  }, [])
+
+  const handleChange = (e) => {
+    setSearch(e.target.value)
+  }
+
+  const filteredCoins = crypto.filter((crypto) =>
+    crypto.name.toLowerCase().includes(search.toLocaleLowerCase())
+  )
+
   return (
-    <div className="App">
-      <h2>crypto tracker</h2>
+    <div className="coin-app">
+      <div className="coin-search">
+        <h2 className="coin-text">search a crypto</h2>
+        <form>
+          <input
+            type="text"
+            className="coin-input"
+            placeholder="search"
+            onChange={handleChange}
+          />
+        </form>
+      </div>
+      {filteredCoins.map((crypto) => {
+        return (
+          <Coin
+            key={crypto.id}
+            name={crypto.name}
+            image={crypto.image}
+            symbol={crypto.symbol}
+            volume={crypto.market_cap}
+            price={crypto.price}
+          />
+        )
+      })}
     </div>
   )
 }
